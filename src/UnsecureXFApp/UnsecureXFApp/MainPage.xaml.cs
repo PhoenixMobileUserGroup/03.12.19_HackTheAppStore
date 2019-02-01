39b069ffd0c8fe8f583dac17a9d9e917ec8fa416
Chris Myers <chris.myers@neudesic.com>
2019-01-31 17:08:07 -0700

    Added XF app

---
+++ src/UnsecureXFApp/UnsecureXFApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UnsecureXFApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string SECURE_STRING = "JcVm98d*=R+ffY9CQ&arr7T92ZoNH27ccQUZJ^,*9Jn7A(}UT.qMf";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            bool isValid = editValidation.Text == SECURE_STRING;
+            txtResult.Text = isValid ? "Valid!" : "Invalid!";
+        }
+    }
+}
